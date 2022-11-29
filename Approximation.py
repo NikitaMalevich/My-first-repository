7b982c81f9574560ffd2dc001e20b7220413c0f2
Nik <102063769+NikitaMalevich@users.noreply.github.com>
2022-11-29 10:21:51 +0300

    Update Approximation.py

--- Approximation.py
+++ Approximation.py
@@ -6,14 +6,18 @@ import numpy as np
 #Table data
 y_in = [1,10,50,160,800]
 x_in = np.linspace(y_in[0], y_in[-1], len(y_in))
+x_approx = np.linspace(y_in[0],y_in[-1],len(y_in)*100)
+
 deg_polynom = 3
 z = np.polyfit(x_in,y_in,deg_polynom)
 p = np.poly1d(z)
 y_out = p(x_in)
+y_approx = p(x_approx)
 
 print(f'\npolynom: {p}')
-plt.plot(x_in,y_out,color='blue',label='polinom')
+plt.plot(x_approx,y_approx,color='blue',label=f'polynom: {p}')
 plt.scatter(x_in,y_in,color='orange',label='initial data')
+
 plt.xlabel('x')
 plt.ylabel('y')
 plt.title('Approximation table data')
